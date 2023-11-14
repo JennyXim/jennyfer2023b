df1b284a43bf3205e018f721d9e7eb34abf83bee
JennyXim <150806167+JennyXim@users.noreply.github.com>
2023-11-14 09:11:31 -0500

    Hola mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
   public static void main (String [] args) {
-         System.out.println("Oceano Oscuridad. "); //display output
+         System.out.println("Oceano Oscuridad. Psycologia oscura programacion "); //display output
    }
 }
